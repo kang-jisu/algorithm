10243997bff1af68dc9e06054f83c5102cacebbf
강지수 <37580216+kang-jisu@users.noreply.github.com>
2022-03-02 00:38:55 +0900

    Create D0301_브루트포스_카펫.java

---
+++ D0301_브루트포스_카펫.java
@@ -0,0 +1,24 @@
+/**
+공식을 찾아서 풀면 되는거였다.
+
+yellow + brown은 총 칸 넓이랑 같고
+yellow는 row-2 * col-2인거.. 
+*/
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+        int S = brown + yellow;
+        
+        for(int i=1; i<=S; i++){
+            int row = i;
+            int col = S/row;
+            if(row>col) continue;
+            if( (row-2)*(col-2) == yellow) {
+                answer[0]=col;
+                answer[1]=row;
+                break;
+            }
+        }
+        return answer;
+    }
+}
